07793244bfa15f9a4ff1510639875c9e48ab7e81
Frank Doyle <frankdoylezw@gmail.com>
2019-03-21 22:18:34 +0000

    Changed HelloWorldController

--- MVCAPP/Controllers/HelloWorldController.cs
+++ MVCAPP/Controllers/HelloWorldController.cs
@@ -16,9 +16,6 @@ namespace MvcMovie.Controllers
         // 
         // GET: /HelloWorld/Welcome/ 
 
-        public string Welcome()
-        {
-            return "This is the Welcome action method...";
-        }
+        public string Welcome(string name, int numTimes = 1) => HtmlEncoder.Default.Encode($"Hello {name}, NumTimes is: {numTimes}");
     }
 }
